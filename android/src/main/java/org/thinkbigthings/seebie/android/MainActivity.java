90fe3119a4ce5274b23c7b0e659675581a30c8ed
Jason Young <thinkbigthings@gmail.com>
2014-02-07 20:00:14 -0500

    can display betterpicker

--- android/src/main/java/org/thinkbigthings/seebie/android/MainActivity.java
+++ android/src/main/java/org/thinkbigthings/seebie/android/MainActivity.java
@@ -3,25 +3,37 @@ package org.thinkbigthings.seebie.android;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.doomonafireball.betterpickers.datepicker.DatePickerBuilder;
+import com.doomonafireball.betterpickers.radialtimepicker.RadialPickerLayout;
+import com.doomonafireball.betterpickers.radialtimepicker.RadialTimePickerDialog;
+import com.doomonafireball.betterpickers.timepicker.TimePickerBuilder;
+import com.doomonafireball.betterpickers.timepicker.TimePickerDialogFragment;
 import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 
 import com.doomonafireball.betterpickers.calendardatepicker.CalendarDatePickerDialog;
 
-public class MainActivity extends Activity {
+import org.joda.time.DateTime;
+
+// TODO figure out how to use betterpickers without the support fragments, would like to use latest fragments
+public class MainActivity extends FragmentActivity implements CalendarDatePickerDialog.OnDateSetListener, RadialTimePickerDialog.OnTimeSetListener {
 
   public final static String SLEEP_SESSION = "org.thinkbigthings.seebie.android.sleepSession";
 
@@ -40,7 +52,33 @@ public class MainActivity extends Activity {
     TimePicker picker2 = (TimePicker) findViewById(R.id.timePicker2);
     picker2.setCurrentHour(4);
     picker2.setCurrentMinute(30);
-    
+
+    Button button = (Button) findViewById(R.id.button);
+
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        RadialTimePickerDialog dialog;
+        dialog = RadialTimePickerDialog.newInstance(MainActivity.this, 21, 30, false);
+        dialog.show(getSupportFragmentManager(), null);
+
+//        DateTime now = DateTime.now();
+//        CalendarDatePickerDialog calendarDatePickerDialog;
+//        calendarDatePickerDialog = CalendarDatePickerDialog
+//            .newInstance(MainActivity.this, now.getYear(), now.getMonthOfYear(), now.getDayOfMonth());
+//        calendarDatePickerDialog.show(getSupportFragmentManager(), "fragment_date_picker_name");
+      }
+    });
+
+  }
+  @Override
+  public void onTimeSet(RadialPickerLayout radialPickerLayout, int i, int i2) {
+
+  }
+  @Override
+  public void onDateSet(CalendarDatePickerDialog dialog, int year, int monthOfYear, int dayOfMonth) {
+    String text = "Year: " + year + "\nMonth: " + monthOfYear + "\nDay: " + dayOfMonth;
   }
 
   @Override
@@ -88,5 +126,6 @@ public class MainActivity extends Activity {
     intent.putExtra(SLEEP_SESSION, session);
     startActivity(intent);
   }
+
 
 }
