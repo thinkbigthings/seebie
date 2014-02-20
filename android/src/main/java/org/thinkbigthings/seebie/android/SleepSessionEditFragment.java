957b4861f554a829a2e71afd9a48616aefd06bac
Jason Young <thinkbigthings@gmail.com>
2014-02-19 19:47:42 -0500

    initial add some fragment classes

---
+++ android/src/main/java/org/thinkbigthings/seebie/android/SleepSessionEditFragment.java
@@ -0,0 +1,157 @@
+package org.thinkbigthings.seebie.android;
+
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.doomonafireball.betterpickers.calendardatepicker.CalendarDatePickerDialog;
+import com.doomonafireball.betterpickers.numberpicker.NumberPickerBuilder;
+import com.doomonafireball.betterpickers.numberpicker.NumberPickerDialogFragment;
+import com.doomonafireball.betterpickers.radialtimepicker.RadialPickerLayout;
+import com.doomonafireball.betterpickers.radialtimepicker.RadialTimePickerDialog;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+
+public class SleepSessionEditFragment  extends Fragment {
+
+
+  protected NumberPickerDialogFragment.NumberPickerDialogHandler awakeInBedCallback = new NumberPickerDialogFragment.NumberPickerDialogHandler() {
+    @Override public void onDialogNumberSet(int reference, int number, double decimal, boolean isNegative, double fullNumber) {
+      currentSession.withMinutesAwakeInBed(number);
+      updateDisplay(currentSession);
+    }
+  };
+  protected NumberPickerDialogFragment.NumberPickerDialogHandler awakeOutOfBedCallback = new NumberPickerDialogFragment.NumberPickerDialogHandler() {
+    @Override public void onDialogNumberSet(int reference, int number, double decimal, boolean isNegative, double fullNumber) {
+      currentSession.withMinutesAwakeOutOfBed(number);
+      updateDisplay(currentSession);
+    }
+  };
+  protected RadialTimePickerDialog.OnTimeSetListener startTimeCallback = new RadialTimePickerDialog.OnTimeSetListener() {
+    @Override public void onTimeSet(RadialPickerLayout radialPickerLayout, int hour, int minute) {
+      currentSession.withStartTime(hour, minute);
+      updateDisplay(currentSession);
+    }
+  };
+  protected RadialTimePickerDialog.OnTimeSetListener finishTimeCallback = new RadialTimePickerDialog.OnTimeSetListener() {
+    @Override public void onTimeSet(RadialPickerLayout radialPickerLayout, int hour, int minute) {
+      currentSession.withFinishTime(hour, minute);
+      updateDisplay(currentSession);
+    }
+  };
+
+  public void onAwakeInBedClick(View view) {
+    NumberPickerBuilder npb = new NumberPickerBuilder()
+        .setFragmentManager(getFragmentManager())
+        .setStyleResId(R.style.BetterPickersDialogFragment)
+        .setLabelText(getResources().getString(R.string.minutes))
+        .setDecimalVisibility(View.GONE)
+        .setPlusMinusVisibility(View.GONE)
+        .addNumberPickerDialogHandler(awakeInBedCallback);
+    npb.show();
+  }
+
+  public void onAwakeOutOfBedClick(View v) {
+    NumberPickerBuilder npb = new NumberPickerBuilder()
+        .setFragmentManager(getFragmentManager())
+        .setStyleResId(R.style.BetterPickersDialogFragment)
+        .setLabelText(getResources().getString(R.string.minutes))
+        .setDecimalVisibility(View.GONE)
+        .setPlusMinusVisibility(View.GONE)
+        .addNumberPickerDialogHandler(awakeOutOfBedCallback);
+    npb.show();
+  }
+
+  public void onStartTimeClick(View view) {
+    LocalTime time= currentSession.getStartTime();
+    RadialTimePickerDialog dialog;
+    dialog = RadialTimePickerDialog.newInstance(startTimeCallback, time.getHourOfDay(), time.getMinuteOfHour(), false);
+    dialog.show(getFragmentManager(), null);
+  }
+
+  protected static class JodaDatePickerDialog extends CalendarDatePickerDialog {
+    public static CalendarDatePickerDialog newInstance(CalendarDatePickerDialog.OnDateSetListener listener, DateTime date) {
+      // JodaTime month is one-based, betterpickers is zero-based like Java Calendar
+      return CalendarDatePickerDialog.newInstance(listener, date.getYear(), date.getMonthOfYear() - 1, date.getDayOfMonth());
+    }
+  };
+
+  protected CalendarDatePickerDialog.OnDateSetListener finishDateCallback = new CalendarDatePickerDialog.OnDateSetListener() {
+    @Override public void onDateSet(CalendarDatePickerDialog calendarDatePickerDialog, int year, int month, int day) {
+      // JodaTime month is one-based, betterpickers is zero-based like Java Calendar
+      currentSession.withFinishDate(year, month + 1, day);
+      updateDisplay(currentSession);
+    }
+  };
+  public void onFinishDateClick(View view) {
+    DateTime time= currentSession.getFinishTime();
+    CalendarDatePickerDialog calendarDatePickerDialog = JodaDatePickerDialog.newInstance(finishDateCallback, time);
+    calendarDatePickerDialog.show(getFragmentManager(), null);
+  }
+
+  public void onFinishTimeClick(View view){
+    DateTime time= currentSession.getFinishTime();
+    RadialTimePickerDialog dialog;
+    dialog = RadialTimePickerDialog.newInstance(finishTimeCallback, time.getHourOfDay(), time.getMinuteOfHour(), false);
+    dialog.show(getFragmentManager(), null);
+  }
+
+  protected SleepSession currentSession;
+
+  public SleepSession getCurrentSession() {
+    return currentSession;
+  }
+
+  public void setCurrentSession(SleepSession session) {
+    currentSession = session;
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_sleep_session_edit, container, false);
+  }
+
+  public View findViewById(int id) {
+    return getView().findViewById(id);
+  }
+
+  private void updateDisplay(SleepSession session) {
+
+    String display;
+    SleepSession.Format formatter = new SleepSession.Format();
+    Resources res = getResources();
+
+    display = String.format(res.getString(R.string._edit_sleep_timeInBedAwake), session.getMinutesAwakeInBed());
+    ((Button) findViewById(R.id.timeInBedAwake)).setText(display);
+
+    display = String.format(res.getString(R.string._edit_sleep_timeOutOfBedAwake), session.getMinutesAwakeOutOfBed());
+    ((Button) findViewById(R.id.timeOutOfBedAwake)).setText(display);
+
+    display = String.format(res.getString(R.string._edit_sleep_startTime),  DateTimeFormat.shortTime().print(session.getStartTime()));
+    ((Button) findViewById(R.id.startTime)).setText(display);
+
+    display = String.format(res.getString(R.string._edit_sleep_finishTime), DateTimeFormat.shortTime().print(session.getFinishTime()));
+    ((Button) findViewById(R.id.finishTime)).setText(display);
+
+    display = String.format(res.getString(R.string._edit_sleep_finishDate), formatter.day(session));
+    ((Button) findViewById(R.id.finishDate)).setText(display);
+
+    TextView totalSleepDisplay = (TextView)findViewById(R.id.total_sleep_display);
+    String[] hrMin = formatter.duration(session);
+    display = String.format(res.getString(R.string._edit_sleep_duration), hrMin[0], hrMin[1]);
+    totalSleepDisplay.setText(display);
+
+    TextView efficiencyDisplay = (TextView)findViewById(R.id.efficiency_display);
+    display = String.format(res.getString(R.string._edit_sleep_efficiency), formatter.efficiency(session));
+    efficiencyDisplay.setText(display);
+  }
+
+}
