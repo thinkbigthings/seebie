d3cab77f8c14d7d56d84309b9269aa2d7dacdad9
Jason Young <thinkbigthings@gmail.com>
2014-02-21 19:50:54 -0500

    modified fragment with callback for communication to activity

--- android/src/main/java/org/thinkbigthings/seebie/android/SleepSessionEditFragment.java
+++ android/src/main/java/org/thinkbigthings/seebie/android/SleepSessionEditFragment.java
@@ -1,6 +1,7 @@
 package org.thinkbigthings.seebie.android;
 
 
+import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,29 +23,27 @@ import org.joda.time.format.DateTimeFormat;
 
 public class SleepSessionEditFragment  extends Fragment {
 
+  private SleepSessionChangeListener mCallback;
+  private SleepSession currentSession;
 
   protected NumberPickerDialogFragment.NumberPickerDialogHandler awakeInBedCallback = new NumberPickerDialogFragment.NumberPickerDialogHandler() {
     @Override public void onDialogNumberSet(int reference, int number, double decimal, boolean isNegative, double fullNumber) {
-      currentSession.withMinutesAwakeInBed(number);
-      updateDisplay(currentSession);
+      update(currentSession.withMinutesAwakeInBed(number));
     }
   };
   protected NumberPickerDialogFragment.NumberPickerDialogHandler awakeOutOfBedCallback = new NumberPickerDialogFragment.NumberPickerDialogHandler() {
     @Override public void onDialogNumberSet(int reference, int number, double decimal, boolean isNegative, double fullNumber) {
-      currentSession.withMinutesAwakeOutOfBed(number);
-      updateDisplay(currentSession);
+      update(currentSession.withMinutesAwakeOutOfBed(number));
     }
   };
   protected RadialTimePickerDialog.OnTimeSetListener startTimeCallback = new RadialTimePickerDialog.OnTimeSetListener() {
     @Override public void onTimeSet(RadialPickerLayout radialPickerLayout, int hour, int minute) {
-      currentSession.withStartTime(hour, minute);
-      updateDisplay(currentSession);
+      update(currentSession.withStartTime(hour, minute));
     }
   };
   protected RadialTimePickerDialog.OnTimeSetListener finishTimeCallback = new RadialTimePickerDialog.OnTimeSetListener() {
     @Override public void onTimeSet(RadialPickerLayout radialPickerLayout, int hour, int minute) {
-      currentSession.withFinishTime(hour, minute);
-      updateDisplay(currentSession);
+      update(currentSession.withFinishTime(hour, minute));
     }
   };
 
@@ -104,7 +103,22 @@ public class SleepSessionEditFragment  extends Fragment {
     dialog.show(getFragmentManager(), null);
   }
 
-  protected SleepSession currentSession;
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+
+    try {
+      mCallback = (SleepSessionChangeListener) activity;
+    } catch (ClassCastException e) {
+      throw new ClassCastException(activity.toString() + " must implement SleepSessionChangeListener");
+    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_sleep_session_edit, container, false);
+  }
+
 
   public SleepSession getCurrentSession() {
     return currentSession;
@@ -114,15 +128,14 @@ public class SleepSessionEditFragment  extends Fragment {
     currentSession = session;
   }
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_sleep_session_edit, container, false);
-  }
-
   public View findViewById(int id) {
     return getView().findViewById(id);
   }
 
+  private void update(SleepSession session) {
+    mCallback.onSessionChanged(currentSession);
+    updateDisplay(session);
+  }
   private void updateDisplay(SleepSession session) {
 
     String display;
@@ -154,4 +167,7 @@ public class SleepSessionEditFragment  extends Fragment {
     efficiencyDisplay.setText(display);
   }
 
+  public interface SleepSessionChangeListener {
+    void onSessionChanged(SleepSession update);
+  }
 }
