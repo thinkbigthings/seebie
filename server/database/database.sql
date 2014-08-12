b03a46803bbc80333e74f2e3114efc8b69fd25f3
Jason Young <thinkbigthings@gmail.com>
2014-08-12 13:43:12 -0400

    saving database for previous commit with new domain object

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
 --
 -- Host: 127.0.0.1    Database: appdb
 -- ------------------------------------------------------
--- Server version	5.5.35-0ubuntu0.13.10.2
+-- Server version	5.5.37-0ubuntu0.13.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,6 +42,33 @@ INSERT INTO `roles` VALUES (2,'ADMIN'),(1,'USER');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `sleep`
+--
+
+DROP TABLE IF EXISTS `sleep`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sleep` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `minutes_awake_in_bed` int(11) NOT NULL,
+  `minutes_awake_not_in_bed` int(11) NOT NULL,
+  `minutes_napping` int(11) NOT NULL,
+  `minutes_total` int(11) NOT NULL,
+  `time_out_of_bed` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sleep`
+--
+
+LOCK TABLES `sleep` WRITE;
+/*!40000 ALTER TABLE `sleep` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sleep` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_role`
 --
 
@@ -67,6 +94,32 @@ LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
 INSERT INTO `user_role` VALUES (10,1),(11,1),(11,2);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_sleep`
+--
+
+DROP TABLE IF EXISTS `user_sleep`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_sleep` (
+  `user_id` bigint(20) NOT NULL,
+  `sleep_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`sleep_id`),
+  KEY `FK_27nkppjx908x0qo55wxf49cot` (`sleep_id`),
+  CONSTRAINT `FK_ark74758v8x0998hytvfynqwy` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_27nkppjx908x0qo55wxf49cot` FOREIGN KEY (`sleep_id`) REFERENCES `sleep` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_sleep`
+--
+
+LOCK TABLES `user_sleep` WRITE;
+/*!40000 ALTER TABLE `user_sleep` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_sleep` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -107,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-17 19:39:41
+-- Dump completed on 2014-08-12 13:43:03
