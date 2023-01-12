a8f907b67d9d322151e68f7cd2202d98efa2c8c0
Jason Young <thinkbigthings@gmail.com>
2023-01-12 15:31:13 -0500

    configure session saving using the recommended spring security 6 approach

--- server/src/main/java/com/seebie/server/security/WebSecurityConfig.java
+++ server/src/main/java/com/seebie/server/security/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package com.seebie.server.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -26,15 +28,18 @@ public class WebSecurityConfig {
                 .map(AntPathRequestMatcher::new)
                 .toList().toArray(new RequestMatcher[paths.size()]);
 
+        var addSessionSaving = new ObjectPostProcessor<BasicAuthenticationFilter>() {
+            public BasicAuthenticationFilter postProcess(BasicAuthenticationFilter filter) {
+                filter.setSecurityContextRepository(new HttpSessionSecurityContextRepository());
+                return filter;
+            }
+        };
+
         http
-            .securityContext(securityContext -> securityContext.requireExplicitSave(false))
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-            .authorizeHttpRequests(authz -> authz
+            .authorizeHttpRequests( customizer -> customizer
                     .requestMatchers(openEndpoints).permitAll()
-                    .anyRequest().authenticated()
-            )
-            .httpBasic()
-                .and()
+                    .anyRequest().authenticated() )
+            .httpBasic(basic -> basic.withObjectPostProcessor(addSessionSaving))
             .csrf()
                 .disable()
             .exceptionHandling()
