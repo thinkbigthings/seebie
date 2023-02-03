cdc0abb3a2fbc543f417e00ae25e272b360da2d2
Jason Young <thinkbigthings@gmail.com>
2023-02-03 05:47:03 -0500

    use sleep form component

--- reactjs/src/CreateSleepSession.js
+++ reactjs/src/CreateSleepSession.js
@@ -3,11 +3,9 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import useApiPost from "./useApiPost";
 import useCurrentUser from "./useCurrentUser";
-import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css';
+import {SleepForm} from "./SleepForm";
 
-
-const numericRegex=/^[0-9]+$/;
 
 const minutesBetween = (date1, date2) => {
 
@@ -49,11 +47,7 @@ function toIsoString(date) {
         ':' + pad(Math.abs(tzo) % 60);
 }
 
-function CreateSleepSession(props) {
-
-
-    const {currentUser} = useCurrentUser();
-
+function createInitSleepData() {
 
     let today = new Date();
     today.setHours(5, 45, 0);
@@ -62,114 +56,58 @@ function CreateSleepSession(props) {
     yesterday.setDate(today.getDate() - 1);
     yesterday.setHours(21, 45, 0);
 
-    const initSleepData = {
+    return {
         startTime: yesterday,
         stopTime: today,
         notes: '',
         outOfBed: 0,
-        tags: []
+        tags: [],
     }
+}
+
+function format(sleepData) {
+    const formattedSleepData = {
+        notes: sleepData.notes,
+        outOfBed: sleepData.outOfBed,
+        tags: sleepData.tags,
+        startTime: toIsoString(sleepData.startTime),
+        stopTime: toIsoString(sleepData.stopTime)
+    }
+    return JSON.stringify(formattedSleepData);
+}
+
+function CreateSleepSession(props) {
+
+    const initSleepData = createInitSleepData();
+
+    const {currentUser} = useCurrentUser();
 
     const sleepUrl = '/user/' + currentUser.username + '/sleep';
 
-
     const post = useApiPost();
     const [showLogSleep, setShowLogSleep] = useState(false);
-    const [sleepData, setSleepData] = useState(initSleepData);
 
-    const onCreate = () => {
-
-        console.log(sleepData);
-
-        const formattedSleepData = {
-            notes: sleepData.notes,
-            outOfBed: sleepData.outOfBed,
-            tags: sleepData.tags,
-            startTime: toIsoString(sleepData.startTime),
-            stopTime: toIsoString(sleepData.stopTime)
-        }
-
-        post(sleepUrl, JSON.stringify(formattedSleepData))
+    const saveData = (data) => {
+        post(sleepUrl, format(data))
             .then(result => setShowLogSleep(false))
             .then(props.onSave);
     }
 
-    function updateSleepSession(updateValues) {
-
-        let updatedSleep = {...sleepData, ...updateValues};
-
-        if( numericRegex.test(updatedSleep.outOfBed)) {
-            setSleepData( updatedSleep );
-        }
-    }
-
-    function onHide() {
-        setSleepData(initSleepData);
+    function hideModal() {
         setShowLogSleep(false);
     }
-
-    function onConfirm() {
-        onCreate();
-        setSleepData(initSleepData);
-    }
-
-    const formDataValid = true;
 
     return (
         <>
             <Button variant="success" onClick={() => setShowLogSleep(true)}>Log Sleep</Button>
 
-            <Modal show={showLogSleep} onHide={onHide} >
+            <Modal show={showLogSleep} onHide={hideModal} >
                 <Modal.Header closeButton>
                     <Modal.Title>Log Sleep</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <div className="mb-3">
-                        <label htmlFor="startTime" className="form-label">Sleep Session Start</label>
-                        <DatePicker
-                            className="form-control" id="startTime" placeholder="Date Start"
-                            dateFormat="MMMM d, yyyy h:mm aa"
-                            showTimeSelect
-                            timeIntervals={15}
-                            timeCaption="time"
-                            timeFormat="p"
-                            selected={sleepData.startTime}
-                            onChange={ date => updateSleepSession({startTime : date })} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="stopTime" className="form-label">Sleep Session End</label>
-                        <DatePicker
-                            className="form-control" id="stopTime" placeholder="Date End"
-                            dateFormat="MMMM d, yyyy h:mm aa"
-                            showTimeSelect
-                            timeIntervals={15}
-                            timeCaption="time"
-                            timeFormat="p"
-                            selected={sleepData.stopTime}
-                            onChange={ date => updateSleepSession({stopTime : date })} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="calculatedMinutes" className="form-label">Time Asleep</label>
-                        <input disabled className="form-control" id="calculatedMinutes" placeholder="Time Asleep"
-                               value={minuteToHrMin(minutesBetween(sleepData.startTime, sleepData.stopTime))} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="outOfBed" className="form-label">Out Of Bed (number of times)</label>
-                        <input type="text"  className="form-control" id="outOfBed" placeholder="Out of Bed (number of times)"
-                               value={sleepData.outOfBed}
-                               onChange={e => updateSleepSession({outOfBed : e.target.value})} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="notes" className="form-label">Notes</label>
-                        <textarea className="form-control" id="notes" placeholder="Notes"
-                               value={sleepData.notes}
-                               onChange={e => updateSleepSession({notes : e.target.value })} />
-                    </div>
+                    <SleepForm onCancel={hideModal} onSave={saveData} initData={initSleepData} />
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={onHide}>Close</Button>
-                    <Button variant="primary" onClick={onConfirm} disabled={!formDataValid}>Save</Button>
-                </Modal.Footer>
             </Modal>
         </>
     );
--- reactjs/src/SleepForm.js
+++ reactjs/src/SleepForm.js
@@ -21,53 +21,16 @@ const minuteToHrMin = (minutes) => {
 
 function SleepForm(props) {
 
+    const {onCancel, onSave, initData} = props;
 
-    const {onCancel, onSave, sleepData} = props;
+    const [sleepData, setSleepData] = useState(initData);
 
-    let today = new Date();
-    today.setHours(5, 45, 0);
+    function updateSleepSession(updateValues) {
 
-    let yesterday = new Date(today.getTime());
-    yesterday.setDate(today.getDate() - 1);
-    yesterday.setHours(21, 45, 0);
+        let updatedSleep = {...sleepData, ...updateValues};
 
-    const displayTimeInit = minuteToHrMin(minutesBetween(yesterday, today));
-
-    // const initialFormData = {
-    //     startDate: ,
-    //     endDate: sleepData.dateAwakened,
-    //     displayTime: displayTimeInit,
-    //     notes: '',
-    //     outOfBed: 0
-    // }
-
-    const [formData, setFormData] = useState(initialFormData);
-
-    const formToDto = (formData) => {
-        return {
-            notes: formData.notes,
-            outOfBed: formData.outOfBed,
-            tags: []
-        }
-    }
-
-    // const dtoToForm = (sleepData) => {
-    //     return {
-    //         endDate: sleepData.dateAwakened,
-    //         startDate: TODO
-    //         notes: sleepData.notes,
-    //         outOfBed: sleepData.outOfBed,
-    //         tags: sleepData.tags
-    //     }
-    // }
-
-    function updateForm(updateValues) {
-
-        let updatedFormData = {...formData, ...updateValues};
-
-        if( numericRegex.test(updatedFormData.outOfBed)) {
-            updatedFormData.displayTime = minuteToHrMin(minutesBetween(updatedFormData.startDate, updatedFormData.endDate));
-            setFormData( updatedFormData );
+        if( numericRegex.test(updatedSleep.outOfBed)) {
+            setSleepData( updatedSleep );
         }
     }
 
@@ -79,52 +42,49 @@ function SleepForm(props) {
                 <div className="mb-3">
                         <label htmlFor="dateStart" className="form-label">Sleep Session Start</label>
                         <DatePicker
-                            className="form-control" id="dateStart" placeholder="Date Start"
+                            className="form-control" id="dateStart" placeholder="Start Time"
                             dateFormat="MMMM d, yyyy h:mm aa"
                             showTimeSelect
-                            timeFormat="HH:mm"
                             timeIntervals={15}
                             timeCaption="time"
                             timeFormat="p"
-                            selected={formData.startDate}
-                            onChange={ date => updateForm({startDate : date })} />
+                            selected={sleepData.startTime}
+                            onChange={ date => updateSleepSession({startTime : date })} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="dateEnd" className="form-label">Sleep Session End</label>
                         <DatePicker
-                            className="form-control" id="dateEnd" placeholder="Date End"
+                            className="form-control" id="dateEnd" placeholder="Stop Time"
                             dateFormat="MMMM d, yyyy h:mm aa"
                             showTimeSelect
-                            timeFormat="HH:mm"
                             timeIntervals={15}
                             timeCaption="time"
                             timeFormat="p"
-                            selected={formData.endDate}
-                            onChange={ date => updateForm({endDate : date })} />
+                            selected={sleepData.stopTime}
+                            onChange={ date => updateSleepSession({stopTime : date })} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="calculatedMinutes" className="form-label">Time Asleep</label>
                         <input disabled className="form-control" id="calculatedMinutes" placeholder="Time Asleep"
-                               value={formData.displayTime} />
+                               value={minuteToHrMin(minutesBetween(sleepData.startTime, sleepData.stopTime))} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="outOfBed" className="form-label">Out Of Bed (number of times)</label>
                         <input type="text"  className="form-control" id="outOfBed" placeholder="Out of Bed (number of times)"
-                               value={formData.outOfBed}
-                               onChange={e => updateForm({outOfBed : e.target.value})} />
+                               value={sleepData.outOfBed}
+                               onChange={e => updateSleepSession({outOfBed : e.target.value})} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="notes" className="form-label">Notes</label>
                         <textarea className="form-control" id="notes" placeholder="Notes"
-                               value={formData.notes}
-                               onChange={e => updateForm({notes : e.target.value })} />
+                               value={sleepData.notes}
+                               onChange={e => updateSleepSession({notes : e.target.value })} />
                     </div>
 
+                <Button variant="success" onClick={() => { onSave(sleepData) }}>Save</Button>
+                <Button variant="light" onClick={ onCancel }>Cancel</Button>
 
-                <Button variant="success" onClick={() => { onSave(formToDto(formData)) }}>Save</Button>
-                <Button variant="light" onClick={onCancel}>Cancel</Button>
-
-                </form>
+            </form>
         </Container>
     );
 }
