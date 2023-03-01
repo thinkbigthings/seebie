f539b3fcd94d74ab2bdd3a2d5d9bcfdb04083776
Jason Young <thinkbigthings@gmail.com>
2023-03-01 06:08:13 -0500

    refactor validation test to use request parameters

--- server/src/test/java/com/seebie/server/controller/ControllerValidationTest.java
+++ server/src/test/java/com/seebie/server/controller/ControllerValidationTest.java
@@ -11,7 +11,6 @@ import com.seebie.server.service.UserService;
 import com.seebie.server.test.support.MvcTestRunner;
 import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,7 +19,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,17 +28,12 @@ import org.springframework.util.MultiValueMap;
 import java.time.ZonedDateTime;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 
 import static com.seebie.server.test.data.TestData.createRandomPersonalInfo;
 import static com.seebie.server.test.data.TestData.createRandomUserRegistration;
+import static com.seebie.server.test.support.MvcTestRunner.EndpointTest;
+import static com.seebie.server.test.support.MvcTestRunner.EndpointTest.*;
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // almost of the full stack is used, and your code will be called in exactly the same way
 // as if it were processing a real HTTP request but without the cost of starting the server
@@ -58,6 +51,9 @@ public class ControllerValidationTest {
 
 	private static final String USERNAME = "someuser";
 	private static final String ADMINNAME = "admin";
+
+	private static final MultiValueMap<String,String> NONE = new LinkedMultiValueMap<>();
+
 
 	@Autowired
 	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
@@ -88,49 +84,34 @@ public class ControllerValidationTest {
 		mvc = new MvcTestRunner(converter);
 	}
 
-	@Test
-	@WithMockUser(username = USERNAME, roles = {"USER"})
-	@DisplayName("Chart Data Access")
-	public void testArguments() throws Exception {
-
-		var from  = ZonedDateTime.now().minusDays(1).format(ISO_OFFSET_DATE_TIME);
-		var to = ZonedDateTime.now().format(ISO_OFFSET_DATE_TIME);
-
-		// TODO test validation and security, do we need to modify all the other tests? We use empty body for GET reqs
-
-		var urlParams = new LinkedMultiValueMap<String, String>();
-		urlParams.put("from", List.of(from));
-		urlParams.put("to", List.of(to));
-
-		// we don't need to url encode the parameters here
-		mockMvc.perform(get("/user/" + USERNAME + "/sleep/chart")
-						.contentType(APPLICATION_JSON)
-						.params(urlParams)
-						.secure(true))
-				.andDo(print())
-				.andExpect(status().is(200));
-
-	}
-
 	private static List<Arguments> provideAdminTestParameters() {
 		return List.of(
 
-				Arguments.of(POST, "/registration", registration, 200),
-				Arguments.of(POST, "/registration", invalidRegistration, 400)
+				Arguments.of(post("/registration", registration), 200),
+				Arguments.of(post("/registration", invalidRegistration), 400)
 		);
 	}
 
 	private static List<Arguments> provideUserTestParameters() {
+
+		// we don't need to url encode the parameters here
+		var fromTo = new LinkedMultiValueMap<String, String>();
+		fromTo.put("from", List.of(ZonedDateTime.now().minusDays(1).format(ISO_OFFSET_DATE_TIME)));
+		fromTo.put("to", List.of(ZonedDateTime.now().format(ISO_OFFSET_DATE_TIME)));
+
 		return List.of(
 
-				Arguments.of(PUT, "/user/" + USERNAME + "/personalInfo", info, 200),
-				Arguments.of(PUT, "/user/" + USERNAME + "/personalInfo", invalidInfo, 400),
+				Arguments.of(put("/user/" + USERNAME + "/personalInfo", info), 200),
+				Arguments.of(put("/user/" + USERNAME + "/personalInfo", invalidInfo), 400),
 
-				Arguments.of(POST, "/user/" + USERNAME + "/sleep", sleepData, 200),
-				Arguments.of(POST, "/user/" + USERNAME + "/sleep", invalidSleepData, 400),
+				Arguments.of(post("/user/" + USERNAME + "/sleep", sleepData), 200),
+				Arguments.of(post("/user/" + USERNAME + "/sleep", invalidSleepData), 400),
 
-				Arguments.of(PUT, "/user/" + USERNAME + "/sleep" + "/1", sleepData, 200),
-				Arguments.of(PUT, "/user/" + USERNAME + "/sleep" + "/1", invalidSleepData, 400)
+				Arguments.of(put("/user/" + USERNAME + "/sleep" + "/1", sleepData), 200),
+				Arguments.of(put("/user/" + USERNAME + "/sleep" + "/1", invalidSleepData), 400),
+
+				Arguments.of(get("/user/" + USERNAME + "/sleep/chart", fromTo), 200),
+				Arguments.of(get("/user/" + USERNAME + "/sleep/chart", NO_PARAM), 400)
 		);
 	}
 
@@ -138,17 +119,16 @@ public class ControllerValidationTest {
 	@MethodSource("provideAdminTestParameters")
 	@WithMockUser(username = ADMINNAME, roles = {"ADMIN"})
 	@DisplayName("Admin Access")
-	void testAdminSecurity(HttpMethod httpMethod, String url, Object reqBody, int expectedStatus) throws Exception {
-		mvc.test(mockMvc, httpMethod, url, reqBody, expectedStatus);
+	void testAdminValidation(EndpointTest test, int expectedStatus) throws Exception {
+		mvc.test(mockMvc, test, expectedStatus);
 	}
 
 	@ParameterizedTest
 	@MethodSource("provideUserTestParameters")
 	@WithMockUser(username = USERNAME, roles = {"USER"})
 	@DisplayName("User Access")
-	void testUserSecurity(HttpMethod httpMethod, String url, Object reqBody, int expectedStatus) throws Exception {
-		mvc.test(mockMvc, httpMethod, url, reqBody, expectedStatus);
+	void testUserSecurity(EndpointTest test, int expectedStatus) throws Exception {
+		mvc.test(mockMvc, test, expectedStatus);
 	}
-
 
 }
--- server/src/test/java/com/seebie/server/test/support/MvcTestRunner.java
+++ server/src/test/java/com/seebie/server/test/support/MvcTestRunner.java
@@ -8,18 +8,53 @@ import com.seebie.server.dto.SleepData;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.util.CollectionUtils.unmodifiableMultiValueMap;
 
 public class MvcTestRunner {
+
+    public record EndpointTest(HttpMethod httpMethod, String url, Object reqBody, MultiValueMap<String, String> reqParams) {
+
+        public static final MultiValueMap<String,String> NO_PARAM = unmodifiableMultiValueMap(new LinkedMultiValueMap<>());
+
+        public static EndpointTest post(String url, Object reqBody) {
+            return new EndpointTest(HttpMethod.POST, url, reqBody, NO_PARAM);
+        }
+        public static EndpointTest put(String url, Object reqBody) {
+            return new EndpointTest(HttpMethod.PUT, url, reqBody, NO_PARAM);
+        }
+        public static EndpointTest get(String url) {
+            return new EndpointTest(HttpMethod.GET, url, "", NO_PARAM);
+        }
+        public static EndpointTest get(String url, MultiValueMap<String, String> reqParams) {
+            return new EndpointTest(HttpMethod.GET, url, "", reqParams);
+        }
+        public static EndpointTest delete(String url) {
+            return new EndpointTest(HttpMethod.DELETE, url, "", NO_PARAM);
+        }
+    }
 
     private ObjectMapper mapper;
 
     public MvcTestRunner(MappingJackson2HttpMessageConverter converter) {
         mapper = converter.getObjectMapper();
+    }
+
+    public void test(MockMvc mvc, EndpointTest test, int expectedStatus) throws Exception {
+        mvc.perform(request(test.httpMethod(), test.url())
+                        .content(toJson(test.reqBody()))
+                        .params(test.reqParams())
+                        .contentType(APPLICATION_JSON)
+                        .secure(true))
+                .andDo(print())
+                .andExpect(status().is(expectedStatus));
     }
 
     public void test(MockMvc mvc, HttpMethod method, String url, Object body, int expectedStatus) throws Exception {
