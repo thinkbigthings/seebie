236ff7f487c5c39f1ec6ecca7d96fc0a833b1eeb
Jason Young <thinkbigthings@gmail.com>
2023-03-06 05:46:29 -0500

    date pickers are integrated with range state

--- reactjs/src/SleepChart.js
+++ reactjs/src/SleepChart.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import {CategoryScale, Chart as ChartJS, Filler, Legend, LinearScale, LineElement, Tooltip, PointElement, Title} from "chart.js";
 import { Line } from 'react-chartjs-2';
@@ -7,6 +7,7 @@ import DatePicker from "react-datepicker";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import SleepDataManager from "./SleepDataManager";
 
 
 ChartJS.register(
@@ -21,35 +22,7 @@ ChartJS.register(
 );
 
 
-// const options = {
-//     responsive: true,
-//     scales: {
-//         y: {
-//             display: true,
-//             stacked: true,
-//             ticks: {
-//                 min: 0, // minimum value
-//                 max: 10 // maximum value
-//             }
-//         }
-//     },
-//     plugins: {
-//         legend: {
-//             position: 'top'
-//         },
-//         title: {
-//             display: true,
-//             text: 'Sleep Duration',
-//         },
-//     },
-// };
-
-
 function SleepChart() {
-
-    // TODO update options https://react-chartjs-2.js.org/components/line/
-
-    // TODO pre-emptively handle this situation https://react-chartjs-2.js.org/docs/working-with-datasets
 
     const labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
@@ -71,41 +44,66 @@ function SleepChart() {
             y: {
                 beginAtZero: true
             }
-        }
+        },
+        plugins: {
+            legend: {
+                position: 'top'
+            },
+            title: {
+                display: true,
+                text: 'Sleep Duration',
+            },
+    },
     };
+
+    let today = new Date();
+
+    let lastMonth = new Date(today.getTime());
+    lastMonth.setDate(today.getDate() - 30);
+    lastMonth.setHours(0, 0, 0);
+
+    const initialRange = {from: lastMonth, to: today};
+    let [range, setRange] = useState(initialRange);
+
+    // TODO update options https://react-chartjs-2.js.org/components/line/
+
+    // TODO pre-emptively handle this situation https://react-chartjs-2.js.org/docs/working-with-datasets
+
+    // TODO check if to is after from
+    function updateSearchRange(updateValues) {
+        let updatedRange = {...range, ...updateValues};
+        setRange(updatedRange);
+    }
+
+    console.log("Fetching chart data");
+    console.log("From " + SleepDataManager.toIsoString(range.from));
+    console.log("To " + SleepDataManager.toIsoString(range.to));
 
     return (
         <Container>
             <Row>
-                <Col>
+                <Col xs={6}></Col>
+
+                <Col xs={1}>
                     <label htmlFor="dateStart">From</label>
                 </Col>
-                <Col>
+                <Col xs={2}>
                     <DatePicker
                         className="form-control" id="dateStart" placeholder="Start Date"
                         dateFormat="MMMM d, yyyy"
-                        // showTimeSelect
-                        // timeIntervals={15}
-                        timeCaption="time"
-                        timeFormat="p"
-                        // selected={data.startTime}
-                        // onChange={ date => onChange({startTime : date })}
+                        selected={range.from}
+                        onChange={ date => updateSearchRange({from : date })}
                     />
                 </Col>
-                <Col xs={6}></Col>
-                <Col>
+                <Col xs={1}>
                     <label htmlFor="dateEnd">To</label>
                 </Col>
-                <Col>
+                <Col xs={2}>
                     <DatePicker
                         className="form-control" id="dateEnd" placeholder="End Date"
                         dateFormat="MMMM d, yyyy"
-                        // showTimeSelect
-                        // timeIntervals={15}
-                        timeCaption="time"
-                        timeFormat="p"
-                        // selected={data.stopTime}
-                        // onChange={ date => onChange({stopTime : date })}
+                        selected={range.to}
+                        onChange={ date => updateSearchRange({to : date })}
                     />
                 </Col>
             </Row>
