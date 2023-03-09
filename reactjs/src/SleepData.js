cace6ef337919ba2fd49c24534dc4dee98f7afb1
Jason Young <thinkbigthings@gmail.com>
2023-03-08 19:37:17 -0500

    List and Chart data should only be rendered (and data fetched) if enclosing tab is the current one

--- reactjs/src/SleepData.js
+++ reactjs/src/SleepData.js
@@ -6,12 +6,15 @@ import SleepChart from "./SleepChart";
 
 function SleepData() {
 
+
+    // the unmountOnExit property on Tabs is set here so that each Tab content component does not make http calls
+    // unless it is actually being rendered
     return (
         <div id="sleep-data-container" className="container mt-3 mb-3">
 
             <CreateSleepSession />
 
-            <Tabs fill defaultActiveKey="list" id="sleep-data-tabs" className="mb-3" >
+            <Tabs unmountOnExit={true} fill defaultActiveKey="list" id="sleep-data-tabs" className="mb-3" >
                 <Tab eventKey="list" title="List">
                     <SleepList />
                 </Tab>
