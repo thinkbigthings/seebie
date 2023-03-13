6e3364cf7c1583024e346f2b7d6587d6abe321c5
Jason Young <thinkbigthings@gmail.com>
2023-03-13 15:39:35 -0400

    Can send out emails!

--- server/src/main/java/com/seebie/server/service/NotificationService.java
+++ server/src/main/java/com/seebie/server/service/NotificationService.java
@@ -2,6 +2,9 @@ package com.seebie.server.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,15 +17,31 @@ public class NotificationService {
 
     private static final Logger LOG = LoggerFactory.getLogger(NotificationService.class);
 
+    private final JavaMailSender emailSender;
     private final UserService userService;
 
-    public NotificationService(UserService userService) {
+    public NotificationService(UserService userService, JavaMailSender emailSender) {
         this.userService = userService;
+        this.emailSender = emailSender;
     }
 
-    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
-    public void reportCurrentTime() {
+    @Scheduled(fixedRate = 60, timeUnit = TimeUnit.SECONDS)
+    public void scanForNotifications() {
 
-        LOG.info("Email would be going out here");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("thinkbigthings@gmail.com");
+        message.setTo("thinkbigthings@gmail.com");
+        message.setSubject("Missing Sleep Log");
+        message.setText("You missed recording your last sleep session...");
+
+        try {
+           // emailSender.send(message);
+            LOG.info("Email went out here");
+
+        }
+        catch(MailException me) {
+            me.printStackTrace();
+        }
+
     }
 }
