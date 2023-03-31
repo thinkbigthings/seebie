96686f664e7b042c15e543f3e3522fedad666afe
Jason Young <thinkbigthings@gmail.com>
2023-03-31 14:45:42 -0400

    tweak the layout

--- reactjs/src/SleepForm.js
+++ reactjs/src/SleepForm.js
@@ -3,6 +3,8 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css';
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 
 function SleepForm(props) {
@@ -34,11 +36,10 @@ function SleepForm(props) {
     }
 
     return (
-        <Container id="userFormId" className="ps-0 " >
-
-            <form>
-
-                <div className="mb-3">
+        <Container id="userFormId" className="ps-0 ">
+            <Row>
+                <Col md={6} className={"pe-4"}>
+                    <div className="mb-3">
                         <label htmlFor="dateStart" className="form-label">Time Fell Asleep</label>
                         <MyDatePicker selectTime={data.startTime} onSelection={ date => onChange({startTime : date })} />
                     </div>
@@ -47,20 +48,25 @@ function SleepForm(props) {
                         <MyDatePicker selectTime={data.stopTime} onSelection={ date => onChange({stopTime : date })} />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="minutesAwake" className="form-label">Time Awake During Sleep Period (minutes)</label>
-                        <input type="text"  className="form-control" id="minutesAwake" placeholder="Time Awake During Sleep Period (minutes)"
+                        <label htmlFor="minutesAwake" className="form-label">Minutes Awake During Sleep Period</label>
+                        <input type="text" className="form-control w-25" id="minutesAwake"
+                               placeholder="Minutes Awake During Sleep Period"
                                value={data.minutesAwake}
                                onChange={e => onChange({minutesAwake : e.target.value})} />
                     </div>
+                </Col>
+                <Col md={6} className={"pe-0"}>
                     <div className="mb-3">
                         <label htmlFor="notes" className="form-label">Notes</label>
-                        <textarea className="form-control" id="notes" placeholder="Notes"
-                               value={data.notes}
-                               onChange={e => onChange({notes : e.target.value })} />
+                        <textarea rows="8" className="form-control" id="notes" placeholder="Notes"
+                                  value={data.notes}
+                                  onChange={e => onChange({notes : e.target.value })} />
                     </div>
+                </Col>
+            </Row>
 
-            </form>
         </Container>
+
     );
 }
 
