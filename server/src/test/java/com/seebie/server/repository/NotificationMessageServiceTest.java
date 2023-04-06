d6d23f6e8f3d23533ecc05723012ff9291e2febc
Jason Young <thinkbigthings@gmail.com>
2023-04-06 06:19:39 -0400

    cleaned up tests

--- server/src/test/java/com/seebie/server/repository/NotificationMessageServiceTest.java
+++ server/src/test/java/com/seebie/server/repository/NotificationMessageServiceTest.java
@@ -49,33 +49,37 @@ public class NotificationMessageServiceTest extends IntegrationTest {
     private static final SleepData firstSleepLog = new SleepData(start.minusHours(8), start);
     private static final int testDurationHours = 72;
 
+    public interface RecordArguments extends Arguments {
+        default Object[] get() {
+            return new Object[] { this };
+        }
+    }
+
     // What's the difference between wrapping the values here vs just having them already unwrapped in the test method?
     // Using a record allows us to name the parameters at the point of declaration instead of at the point of use.
-    record TestParameters(boolean notificationsEnabled, int logSleepEveryNumHours, int expectedNotificationCount) {}
+    // It's easier to see which value is which in the IDE, and consolidates the number of arguments to the test method.
+    record TestParams(boolean notificationsEnabled, int sleepLogFrequencyHrs, int expectedNotificationCount) implements RecordArguments { }
 
     private static List<Arguments> provideSleepLogParameters() {
         return List.of(
-                Arguments.of(new TestParameters(true, 12, 0)),
-                Arguments.of(new TestParameters(true, 24, 0)),
-                Arguments.of(new TestParameters(true, 48, 1)),
-                Arguments.of(new TestParameters(true, 96, 2)),
+            new TestParams(true, 12, 0),
+            new TestParams(true, 24, 0),
+            new TestParams(true, 48, 1),
+            new TestParams(true, 96, 2),
 
-                Arguments.of(new TestParameters(false, 12, 0)),
-                Arguments.of(new TestParameters(false, 24, 0)),
-                Arguments.of(new TestParameters(false, 48, 0)),
-                Arguments.of(new TestParameters(false, 96, 0))
+            new TestParams(false, 12, 0),
+            new TestParams(false, 24, 0),
+            new TestParams(false, 48, 0),
+            new TestParams(false, 96, 0)
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideSleepLogParameters")
-    public void testScanForNotifications(TestParameters params) {
-
-        String userPrefix = String.join( "-", "notify",
-                Boolean.toString(params.notificationsEnabled()),
-                Integer.toString(params.logSleepEveryNumHours()) );
+    public void testScanForNotifications(TestParams params) {
 
         // create new user
+        String userPrefix = "notify-" + params.notificationsEnabled();
         RegistrationRequest testUserRegistration = TestData.createRandomUserRegistration(userPrefix);
         userService.saveNewUser(testUserRegistration);
         String username = testUserRegistration.username();
@@ -87,14 +91,13 @@ public class NotificationMessageServiceTest extends IntegrationTest {
 
         userService.updateUser(username, updatedInfo);
 
-        // save sleep session
+        // save first sleep session
         var firstSleepData = sleepService.saveNew(username, firstSleepLog).sleepData();
-
 
         long numNotifications = 0L;
         for(int hoursPassed = 0; hoursPassed < testDurationHours; hoursPassed++) {
 
-            if(hoursPassed % params.logSleepEveryNumHours() == 0) {
+            if(hoursPassed % params.sleepLogFrequencyHrs() == 0) {
                 var nextSleep = TestData.increment(firstSleepData, Duration.ofHours(hoursPassed));
                 sleepService.saveNew(username, nextSleep).sleepData();
             }
