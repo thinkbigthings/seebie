6a20940dc1d240115ddba17827c42fca6c25fba9
Jason Young <thinkbigthings@gmail.com>
2023-04-28 06:08:11 -0400

    fixed migration

--- server/src/main/resources/db/migration/V9__remember_me_indexes.sql
+++ server/src/main/resources/db/migration/V9__remember_me_indexes.sql
@@ -2,4 +2,4 @@
 set search_path TO public;
 
 CREATE INDEX index_persistent_logins_series ON persistent_logins (series);
-CREATE INDEX index_persistent_logins_username ON SPRING_SESSION (username);
+CREATE INDEX index_persistent_logins_username ON persistent_logins (username);
