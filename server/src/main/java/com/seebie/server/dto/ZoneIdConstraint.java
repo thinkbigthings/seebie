ce8524dfbea347c259573ad6693df561d58f755d
Jason Young <thinkbigthings@gmail.com>
2023-06-15 06:00:32 -0400

    removed component annotation

--- server/src/main/java/com/seebie/server/dto/ZoneIdConstraint.java
+++ server/src/main/java/com/seebie/server/dto/ZoneIdConstraint.java
@@ -2,11 +2,9 @@ package com.seebie.server.dto;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import org.springframework.stereotype.Component;
 
 import java.lang.annotation.*;
 
-@Component
 @Documented
 @Constraint(validatedBy = ZoneIdValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD })
