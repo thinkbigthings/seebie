5e3958a73c8681c0624e056e6d6580e1ba2df8de
Jason Young <thinkbigthings@gmail.com>
2023-06-27 04:24:46 +0800

    refactoring

--- reactjs/src/CreateSleepSession.js
+++ reactjs/src/CreateSleepSession.js
@@ -28,21 +28,6 @@ function CreateSleepSession(props) {
             .then(props.onSave);
     }
 
-    function updateSleepSession(updateValues) {
-
-        let updatedSleep = {...sleepData, ...updateValues};
-
-        // use the local time without the offset for display purposes
-        let localStartTime = SleepDataManager.toIsoString(updatedSleep.localStartTime).substring(0, 19);
-        let localStopTime = SleepDataManager.toIsoString(updatedSleep.localStopTime).substring(0, 19);
-        updatedSleep.startTime = localStartTime + sleepData.startTime.substring(19);
-        updatedSleep.stopTime = localStopTime + sleepData.stopTime.substring(19);
-
-        if(SleepDataManager.isDataValid(updatedSleep)) {
-            setSleepData(updatedSleep);
-        }
-    }
-
     return (
         <>
             <Button variant="secondary" className="px-4 py-2" onClick={() => setShowModal(true)}>
@@ -54,7 +39,7 @@ function CreateSleepSession(props) {
                     <Modal.Title>Log Sleep</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <SleepForm onChange={updateSleepSession} data={sleepData} />
+                    <SleepForm setSleepData={setSleepData} sleepData={sleepData} />
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={ () => setShowModal(false) }>Cancel</Button>
