dc963232008e0d7b6495bce70ed47d7c471cab0a
Jason Young <thinkbigthings@gmail.com>
2023-08-26 20:10:37 -0400

    simplify mapper

--- server/src/test/java/com/seebie/server/test/data/DtoJsonMapper.java
+++ server/src/test/java/com/seebie/server/test/data/DtoJsonMapper.java
@@ -2,10 +2,6 @@ package com.seebie.server.test.data;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.seebie.server.dto.HistogramRequest;
-import com.seebie.server.dto.PersonalInfo;
-import com.seebie.server.dto.RegistrationRequest;
-import com.seebie.server.dto.SleepData;
 
 import java.util.function.Function;
 
@@ -13,28 +9,16 @@ import java.util.function.Function;
  * The test request object takes a string, so we need to format objects ahead of time
  * to send a request body into the app.
  */
-public class DtoJsonMapper implements Function<Object, String> {
-
-    private ObjectMapper mapper;
-
-    public DtoJsonMapper(ObjectMapper mapper) {
-        this.mapper = mapper;
-    }
+public record DtoJsonMapper(ObjectMapper mapper) implements Function<Object, String> {
 
     @Override
     public String apply(Object dto) {
+
         try {
-            return switch (dto) {
-                case String s -> s;
-                case PersonalInfo p -> mapper.writerFor(p.getClass()).writeValueAsString(p);
-                case RegistrationRequest r -> mapper.writerFor(r.getClass()).writeValueAsString(r);
-                case SleepData d -> mapper.writerFor(d.getClass()).writeValueAsString(d);
-                case HistogramRequest r -> mapper.writerFor(r.getClass()).writeValueAsString(r);
-                default -> throw new IllegalArgumentException("Can't find object mapper for " + dto + "you need to update DtoJsonMapper");
-            };
+            return mapper.writerFor(dto.getClass()).writeValueAsString(dto);
         }
         catch (JsonProcessingException e) {
-            throw new IllegalArgumentException("Can't write value for " + dto);
+            throw new RuntimeException("Can't write value for " + dto, e);
         }
     }
 }
