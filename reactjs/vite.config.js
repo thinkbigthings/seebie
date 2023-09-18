298eba5bb34e73ab613b270f6968053325eaeee2
Jason Young <thinkbigthings@gmail.com>
2023-09-18 06:04:39 -0400

    fixed https issue but still cant hit backend

--- reactjs/vite.config.js
+++ reactjs/vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig(() => {
     return {
         build: {
             outDir: 'build',
         },
-        plugins: [react()],
-        // proxy: {
-        //     // string shorthand: http://localhost:5173/ -> http://localhost:4567/foo
-        //     '/login': 'https://localhost:9000/login',
-        // }
+        plugins: [react(), basicSsl()],
+        proxy: {
+            '/': {
+                target: 'https://localhost:9000',
+                changeOrigin: true,
+                secure: false, // This allows requests to be proxied to servers with self-signed certificates
+            },
+
+        },
     }
 });
