f92c776232526ef3233dc70121defeb7add7ccc6
Jason Young <thinkbigthings@gmail.com>
2023-11-18 14:14:21 -0500

    works for both tests and boottestrun

--- server/src/test/java/com/seebie/server/test/IntegrationTest.java
+++ server/src/test/java/com/seebie/server/test/IntegrationTest.java
@@ -4,9 +4,7 @@ import com.seebie.server.PropertyLogger;
 import com.seebie.server.service.SleepService;
 import com.seebie.server.service.UserService;
 import com.seebie.server.test.data.TestDataPopulator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.TestInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
@@ -47,18 +45,15 @@ public class IntegrationTest {
     public static class TestConfig {
 
         @Bean public MailSender createMailSenderToLogs() {
-            LOG.info("Using a mail sender that logs to the console instead of sending emails.");
             return new MailSenderToLogs();
         }
 
         @Bean public PropertyLogger createPropertyLogger(ConfigurableEnvironment env) {
-            LOG.info("Log all properties for debugging, including sensitive values, so don't use in production");
             return new PropertyLogger(env);
         }
 
         @Bean
         public FlywayMigrationStrategy clean() {
-            LOG.info("Cleaning database for integration tests so we don't have state from previous runs");
             return flyway -> {
                 flyway.clean();
                 flyway.migrate();
@@ -67,35 +62,20 @@ public class IntegrationTest {
 
         @Bean
         public TestDataPopulator createTestDataPopulator(UserService userService, SleepService sleepService) {
-            LOG.info("Populating test data for integration tests.");
             return new TestDataPopulator(userService, sleepService);
         }
 
-//        @Bean
-//        @ServiceConnection
-//        public PostgreSQLContainer<?> postgres() {
-//            LOG.info("Creating a PostgreSQL container for integration tests.");
-//            return new PostgreSQLContainer<>("postgres:15.4");
-//        }
-
+        @Bean
+        @ServiceConnection
+        public PostgreSQLContainer<?> postgresForBootTestRun() {
+            return new PostgreSQLContainer<>("postgres:15.4").withDatabaseName("testconfig");
+        }
     }
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.4");
+    static PostgreSQLContainer<?> postgresForTests = new PostgreSQLContainer<>("postgres:15.4").withDatabaseName("inttest");
 
-
-    @BeforeEach
-    public void startup(TestInfo testInfo) {
-
-        LOG.info("");
-        LOG.info("=======================================================================================");
-        LOG.info("Executing test " + testInfo.getDisplayName());
-        LOG.info("TestContainer username: " + postgres.getUsername());
-        LOG.info("TestContainer password: " + postgres.getPassword());
-        LOG.info("TestContainer jdbc url: " + postgres.getJdbcUrl());
-        LOG.info("");
-    }
 
     public static class MailSenderToLogs implements MailSender {
 
