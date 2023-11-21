fc2c3b544d875ba5c65aaea5db09c716839e5ede
Jason Young <thinkbigthings@gmail.com>
2023-11-21 18:19:17 -0500

    update tests 1 of 3

--- server/src/test/java/com/seebie/server/controller/EndToEndIntegrationTest.java
+++ server/src/test/java/com/seebie/server/controller/EndToEndIntegrationTest.java
@@ -1,46 +1,26 @@
 package com.seebie.server.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seebie.server.dto.PersonalInfo;
 import com.seebie.server.dto.RegistrationRequest;
 import com.seebie.server.dto.User;
 import com.seebie.server.dto.UserSummary;
 import com.seebie.server.service.UserService;
 import com.seebie.server.test.IntegrationTest;
-import com.seebie.server.test.client.ApiClientStateful;
-import com.seebie.server.test.client.BasicAuthenticator;
-import com.seebie.server.test.client.InsecureTrustManager;
 import com.seebie.server.test.client.ParsablePage;
+import com.seebie.server.test.client.RestClientFactory;
 import com.seebie.server.test.data.TestData;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.client.RestClientSsl;
-import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
-import org.springframework.boot.web.client.RestClientCustomizer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import java.net.CookieManager;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -61,91 +41,11 @@ public class EndToEndIntegrationTest extends IntegrationTest {
     private static URI testUserUrl;
     private static URI testUserUpdatePasswordUrl;
 
-    private ObjectMapper mapper;
-
-    private static ApiClientStateful adminClient;
-    private static ApiClientStateful userClient;
-
-    public static HttpClient unAuthClient(SSLContext sslContext) {
-        return HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(300))
-                .cookieHandler(new CookieManager())
-                .sslContext(sslContext)
-                .build();
-    }
-
-    public static HttpClient basicAuth(String username, String password, SSLContext sslContext) {
-        return HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(300))
-                .cookieHandler(new CookieManager())
-                .authenticator(new BasicAuthenticator(username, password))
-                .sslContext(sslContext)
-                .build();
-    }
-
-    public static HttpClient removeBasicAuth(HttpClient client) {
-        return HttpClient.newBuilder()
-                .connectTimeout(client.connectTimeout().get())
-                .cookieHandler(client.cookieHandler().get())
-                .sslContext(client.sslContext())
-                .build();
-    }
-
-
-    protected static SSLContext createSsl() {
-        try {
-            // don't check certificates so we can use self-signed certs
-            SSLContext sc = SSLContext.getInstance("TLSv1.3");
-            sc.init(null, new TrustManager[]{new InsecureTrustManager()}, new SecureRandom());
-            return sc;
-        }
-        catch (NoSuchAlgorithmException | KeyManagementException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static RestClientFactory clientFactory;
 
     @BeforeAll
-    public static void setupClient(@Autowired RestClient.Builder restClientBuilder, @LocalServerPort int randomPort) {
-
-        var insecureContext = createSsl();
-
-        // would like to customize it at startup with a RestClientCustomizer or ObjectProvider<RestClientCustomizer>
-        // but couldn't find out to wire it
-        var baseUrl = STR."https://localhost:\{randomPort}";
-        restClientBuilder.baseUrl(baseUrl);
-
-
-        // I thought the existing bundle would work to configure ssl here,
-        // but it fails with "unable to find valid certification path to requested target"
-        // whether I use it to create an SslContext or use RestClient.Builder.apply(ssl.fromBundle("appbundle"))
-        var basicAuth = basicAuth("admin", "admin", insecureContext);
-
-        var basicRestClient = restClientBuilder.clone()
-                                            .requestFactory(new JdkClientHttpRequestFactory(basicAuth))
-                                            .build();
-
-        basicRestClient.get()
-                .uri("/api/login")
-                .retrieve()
-                .body(String.class);
-
-
-        var restClient = restClientBuilder.clone()
-                                            .requestFactory(new JdkClientHttpRequestFactory(removeBasicAuth(basicAuth)))
-                                            .build();
-
-        var userResponse = restClient.get()
-                .uri("/api/user/admin")
-                .retrieve()
-                .body(String.class);
-
-        System.out.println("userResponse = " + userResponse);
-    }
-
-    @BeforeEach
-    public void setup(@Autowired MappingJackson2HttpMessageConverter converter) {
-        // use the actual mapper configured in the application
-        mapper = converter.getObjectMapper();
+    public static void testClient(@Autowired RestClient.Builder builder, @LocalServerPort int randomPort) {
+        clientFactory = new RestClientFactory(builder, randomPort);
     }
 
     @BeforeAll
@@ -159,9 +59,6 @@ public class EndToEndIntegrationTest extends IntegrationTest {
         baseUrl = STR."https://localhost:\{randomServerPort}/api/";
         users = URI.create(baseUrl + "user");
 
-        adminClient = new ApiClientStateful(baseUrl, "admin", "admin");
-
-
         RegistrationRequest testUserRegistration = TestData.createRandomUserRegistration();
         userService.saveNewUser(testUserRegistration);
 
@@ -169,16 +66,18 @@ public class EndToEndIntegrationTest extends IntegrationTest {
         testUserPassword = testUserRegistration.plainTextPassword();
         testUserUrl = URI.create(users + "/" + testUserName);
         testUserUpdatePasswordUrl = URI.create(testUserUrl + "/password/update");
-
-        userClient = new ApiClientStateful(baseUrl, testUserName, testUserPassword);
     }
 
     @Test()
     @DisplayName("Admin list users")
-    public void adminListUsers() throws JsonProcessingException {
+    public void adminListUsers() {
 
-        String results = adminClient.get(users);
-        Page<UserSummary> page = mapper.readValue(results, new TypeReference<ParsablePage<UserSummary>>() {});
+        RestClient admin = clientFactory.createLoggedInClient("admin", "admin");
+
+        Page<UserSummary> page = admin.get()
+                .uri(users)
+                .retrieve()
+                .body(new ParameterizedTypeReference<ParsablePage<UserSummary>>(){});
 
         assertTrue(page.isFirst());
         assertTrue(page.getTotalElements() >= 1);
@@ -188,12 +87,15 @@ public class EndToEndIntegrationTest extends IntegrationTest {
     @DisplayName("Update user password")
     public void testUpdatePassword() {
 
-        PersonalInfo info = userClient.get(testUserUrl, User.class).personalInfo();
-        String newPassword = "password";
-        userClient.post(testUserUpdatePasswordUrl, newPassword);
-        userClient = new ApiClientStateful(baseUrl, testUserName, newPassword);
+        RestClient user = clientFactory.createLoggedInClient(testUserName, testUserPassword);
+        PersonalInfo info = user.get().uri(testUserUrl).retrieve().body(User.class).personalInfo();
 
-        PersonalInfo info2 = userClient.get(testUserUrl, User.class).personalInfo();
+        String newPassword = testUserPassword + "1";
+        user.post().uri(testUserUpdatePasswordUrl).body(newPassword).retrieve();
+
+        user = clientFactory.createLoggedInClient(testUserName, newPassword);
+        PersonalInfo info2 = user.get().uri(testUserUrl).retrieve().body(User.class).personalInfo();
+
         assertEquals(info, info2);
     }
 }
--- server/src/test/java/com/seebie/server/test/client/RestClientFactory.java
+++ server/src/test/java/com/seebie/server/test/client/RestClientFactory.java
@@ -1,4 +1,96 @@
 package com.seebie.server.test.client;
 
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import java.net.CookieManager;
+import java.net.http.HttpClient;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
+
 public class RestClientFactory {
+
+    private final RestClient.Builder restClientBuilder;
+
+    private static final SSLContext insecureContext = createInsecureSsl();
+
+    public RestClientFactory(RestClient.Builder restClientBuilder, int randomPort) {
+
+        // would like to customize the builder at system startup time
+        // with a RestClientCustomizer or ObjectProvider<RestClientCustomizer>
+        // but couldn't find out to wire it
+
+        var baseUrl = STR."https://localhost:\{randomPort}";
+        this.restClientBuilder = restClientBuilder.clone().baseUrl(baseUrl);
+    }
+
+    public RestClient createUnAuthClient() {
+        return restClientBuilder.clone()
+                .requestFactory(new JdkClientHttpRequestFactory(unAuthClient()))
+                .build();
+    }
+
+    public RestClient createLoggedInClient(String username, String plainTextPassword) {
+
+            // I thought the existing ssl bundle would work to configure ssl here,
+            // but whether I use it to create an SslContext or use RestClient.Builder.apply(ssl.fromBundle("appbundle"))
+            // it fails with "unable to find valid certification path to requested target"
+            // so need to use an insecure truststore to work with self-signed certs
+            var basicAuth = basicAuth(username, plainTextPassword);
+            var basicRestClient = restClientBuilder.clone()
+                    .requestFactory(new JdkClientHttpRequestFactory(basicAuth))
+                    .build();
+
+            basicRestClient.get()
+                    .uri("/api/login")
+                    .retrieve()
+                    .body(String.class);
+
+            var sessionAuth = removeBasicAuth(basicAuth);
+            return restClientBuilder.clone()
+                    .requestFactory(new JdkClientHttpRequestFactory(sessionAuth))
+                    .build();
+    }
+
+    private static HttpClient unAuthClient() {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(300))
+                .cookieHandler(new CookieManager())
+                .sslContext(insecureContext)
+                .build();
+    }
+
+    private static HttpClient basicAuth(String username, String password) {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(300))
+                .cookieHandler(new CookieManager())
+                .authenticator(new BasicAuthenticator(username, password))
+                .sslContext(insecureContext)
+                .build();
+    }
+
+    private static HttpClient removeBasicAuth(HttpClient client) {
+        return HttpClient.newBuilder()
+                .connectTimeout(client.connectTimeout().get())
+                .cookieHandler(client.cookieHandler().get())
+                .sslContext(client.sslContext())
+                .build();
+    }
+
+    private static SSLContext createInsecureSsl() {
+        try {
+            // don't check certificates so we can use self-signed certs
+            SSLContext sc = SSLContext.getInstance("TLSv1.3");
+            sc.init(null, new TrustManager[]{new InsecureTrustManager()}, new SecureRandom());
+            return sc;
+        }
+        catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
