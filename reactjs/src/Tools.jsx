b4252303bdf64d3f8135447e65162e86a3b5aff4
Jason Young <thinkbigthings@gmail.com>
2023-12-28 20:38:20 -0500

    show valid and invalid styles in tools page

--- reactjs/src/Tools.jsx
+++ reactjs/src/Tools.jsx
@@ -5,7 +5,6 @@ import Button from "react-bootstrap/Button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faDownload, faUpload} from "@fortawesome/free-solid-svg-icons";
 import Form from 'react-bootstrap/Form';
-import Row from "react-bootstrap/Row";
 import useHttpError from "./hooks/useHttpError";
 import {useApiGet} from "./hooks/useApiGet";
 import {useParams} from "react-router-dom";
@@ -13,42 +12,31 @@ import SuccessModal from "./component/SuccessModal";
 
 function Tools() {
 
-    const {username} = useParams();
+    const { username } = useParams();
+    const { throwOnHttpError } = useHttpError();
 
-    const {throwOnHttpError} = useHttpError();
-
-    const [selectedFile, setSelectedFile] = useState({});
-    const [isFilePicked, setIsFilePicked] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
     const [showSuccessModal, setShowSuccessModal] = useState(false);
-    const [uploadSuccessInfo, setUploadSuccessInfo] = useState({numImported: 0, username: username});
+    const [uploadSuccessInfo, setUploadSuccessInfo] = useState({ numImported: 0, username: username });
 
-    const downloadUrl = '/api/user/' + username + '/sleep/download';
-    const uploadUrl = '/api/user/' + username + '/sleep/upload';
+    const downloadUrl = `/api/user/${username}/sleep/download`;
+    const uploadUrl = `/api/user/${username}/sleep/upload`;
+    const sleepUrl = `/api/user/${username}/sleep`;
 
-    const sleepUrl = '/api/user/' + username + '/sleep';
-
-    // so we can retrieve the total number of records for the user
     const [data] = useApiGet(sleepUrl, 1, 0);
-    const numUserRecords = data.totalElements;
+    const numSleepRecords = data.totalElements;
 
     const onFilePicked = (event) => {
         setSelectedFile(event.target.files[0]);
-        setIsFilePicked(true);
-    }
-
-
-    const isCsv = isFilePicked && selectedFile.type === "text/csv";
-
+    };
 
     const onUploadSuccess = (uploadResponse) => {
         setUploadSuccessInfo(uploadResponse);
         setShowSuccessModal(true);
-        setSelectedFile({});
-        setIsFilePicked(false);
-    }
+        setSelectedFile(null);
+    };
 
-    function handleSubmission() {
-
+    const handleSubmission = () => {
         const formData = new FormData();
         formData.append('file', selectedFile);
 
@@ -59,16 +47,23 @@ function Tools() {
             body: formData
         };
 
-        return fetch(uploadUrl, requestMeta)
+        fetch(uploadUrl, requestMeta)
             .then(throwOnHttpError)
             .then((response) => response.json())
             .then(onUploadSuccess)
             .catch((error) => console.error('Error:', error));
-    }
+    };
+
+    const isCsv = selectedFile && selectedFile.type === "text/csv";
+
+    const showValidMessage = isCsv && selectedFile;
+    const showInvalidMessage = !isCsv && selectedFile;
+    const validStyle = showValidMessage ? "was-validated" : "";
+
+    console.log(validStyle);
 
     return (
         <Container>
-
             <SuccessModal title="Upload Success" showing={showSuccessModal} handleClose={() => setShowSuccessModal(false)}>
                 Uploaded {uploadSuccessInfo.numImported} records for user {username}.
             </SuccessModal>
@@ -78,10 +73,10 @@ function Tools() {
             <hr />
 
             <Container className="mx-0 px-0 my-5">
-                 <h4 className="mb-3" >Export</h4>
-                <label className="d-block mb-3">You have {numUserRecords} sleep records that you can download to CSV</label>
+                <h4 className="mb-3">Export</h4>
+                <label className="d-block mb-3">You have {numSleepRecords} sleep records that you can download to CSV</label>
                 <a href={downloadUrl}>
-                    <Button variant="secondary" >
+                    <Button variant="secondary">
                         <FontAwesomeIcon className="app-highlight me-2" icon={faDownload} />
                         Download
                     </Button>
@@ -91,39 +86,25 @@ function Tools() {
             <hr />
 
             <Container className="mx-0 px-0 my-5">
-                <h4 className="mb-3" >Import</h4>
+                <h4 className="mb-3">Import</h4>
 
-                <Form.Group controlId="formFile" className="mb-3">
-                    <Form.Label>Select CSV file with sleep data to upload</Form.Label>
-                    <Form.Control type="file" name={"file"} onChange={onFilePicked} />
-                    {
-                        (isFilePicked) && (! isCsv) ?
-                            <Container>
-                                <Row>You must select a CSV file</Row>
-                            </Container>
-                        : ""
-                    }
-                    {
-                        (isFilePicked && isCsv) ?
-                            <Container>
-                                <Row>File: {selectedFile.name} </Row>
-                                <Row>Last Modified: {selectedFile.lastModifiedDate.toLocaleDateString()}</Row>
-                                <Row>Size: {selectedFile.size} bytes </Row>
-                            </Container>
-                        : ""
-                    }
-                </Form.Group>
+                <Form>
+                    <Form.Group controlId="formFile" className={validStyle + " mb-3"}>
+                        <Form.Label>Select CSV file with sleep data to upload</Form.Label>
+                        <Form.Control type="file" name="file" onChange={onFilePicked} isInvalid={showInvalidMessage} />
+                        <Form.Control.Feedback type="invalid">
+                            Please select a CSV file.
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-                <Button variant="secondary" onClick={handleSubmission} disabled={ (!isFilePicked) || (!isCsv) } >
-                    <FontAwesomeIcon className="app-highlight me-2" icon={faUpload} />
-                    Upload
-                </Button>
-
+                    <Button variant="secondary" onClick={handleSubmission} disabled={!isCsv}>
+                        <FontAwesomeIcon className="app-highlight me-2" icon={faUpload} />
+                        Upload
+                    </Button>
+                </Form>
             </Container>
-
         </Container>
     );
 }
-
 
 export default Tools;
