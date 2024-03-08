0eb5b6ee5d964a993649b2ccb0e3a3ee0ee865b6
Jason Young <thinkbigthings@gmail.com>
2024-03-08 06:31:33 -0500

    exclude Application from coverage

--- server/src/test/java/com/seebie/server/ApplicationTest.java
+++ server/src/test/java/com/seebie/server/ApplicationTest.java
@@ -3,6 +3,9 @@ package com.seebie.server;
 import com.seebie.server.test.IntegrationTest;
 import org.springframework.boot.SpringApplication;
 
+/**
+ * For use with springBootTest, this class is used to run the application in a test context.
+ */
 public class ApplicationTest {
 
     public static void main(String[] args) {
