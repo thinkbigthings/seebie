787fa3da1b3dbcc6eb9a67e904d15caf005a72a3
Jason Young <thinkbigthings@gmail.com>
2024-04-28 05:24:59 -0400

    cleaned up config

--- server/src/main/java/com/seebie/server/security/WebSecurityConfig.java
+++ server/src/main/java/com/seebie/server/security/WebSecurityConfig.java
@@ -1,45 +1,35 @@
 package com.seebie.server.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.RememberMeServices;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
 
-    public static final String API_LOGIN = "/api/login";
     private static Logger LOG = LoggerFactory.getLogger(WebSecurityConfig.class);
+
+    public static final String API_LOGIN = "/api/login";
+    public static final String API_LOGOUT = "/api/logout";
 
     public static final String SESSION_COOKIE = "SESSION";
     public static final String REMEMBER_ME_COOKIE = "remember-me";
     public static final String COOKIE = "Cookie";
-
-    // TODO This can be replaced with a simpler API call as of Spring Security 6.1.0
-    // See https://github.com/spring-projects/spring-security/issues/12031
-    private static class BasicAuthPostProcessor implements ObjectPostProcessor<BasicAuthenticationFilter> {
-        @Override
-        public <O extends BasicAuthenticationFilter> O postProcess(O filter) {
-            filter.setSecurityContextRepository(new HttpSessionSecurityContextRepository());
-            return filter;
-        }
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, RememberMeServices rememberMeServices) throws Exception {
@@ -48,39 +38,36 @@ public class WebSecurityConfig {
         var openEndpoints = paths.stream().map(AntPathRequestMatcher::new).toList().toArray(new RequestMatcher[paths.size()]);
 
         http
-            .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+            .requiresChannel(channel -> channel
+                    .anyRequest().requiresSecure())
             .authorizeHttpRequests(customizer -> customizer
                     .requestMatchers(openEndpoints).permitAll()
                     .anyRequest().authenticated())
-            .httpBasic(basic -> basic.withObjectPostProcessor(new BasicAuthPostProcessor()))
-            .exceptionHandling(exceptions -> exceptions
-                    .defaultAuthenticationEntryPointFor(
-                            new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
-                            new AntPathRequestMatcher(API_LOGIN)
-                    )
-                    .authenticationEntryPoint(
-                            (req, resp, authException) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
-                    ))
-            .requestCache(cache -> cache.requestCache(new NullRequestCache()))
+            .httpBasic(basic -> basic
+                    .securityContextRepository(new HttpSessionSecurityContextRepository()))
+            .requestCache(cache -> cache
+                    .requestCache(new NullRequestCache()))
             .sessionManagement(session -> session
-                    .invalidSessionStrategy((request, response) -> {
-                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                        response.getWriter().write("Session expired or invalid");
-                        LOG.info(STR."Session expired or invalid. Request cookie: \{request.getHeader(COOKIE)}");
-                    })
+                    .invalidSessionStrategy(this::invalidSession)
                     .maximumSessions(1)
                     .maxSessionsPreventsLogin(true))
-            .csrf((csrf) -> csrf.disable())
+            .csrf((csrf) -> csrf
+                    .disable())
             .logout(config -> config
-                    .logoutUrl("/api/logout")
+                    .logoutUrl(API_LOGOUT)
                     .invalidateHttpSession(true)
                     .clearAuthentication(true)
-                    .deleteCookies(SESSION_COOKIE, REMEMBER_ME_COOKIE)
-            )
+                    .deleteCookies(SESSION_COOKIE, REMEMBER_ME_COOKIE))
             .rememberMe(config -> config
                     .rememberMeServices(rememberMeServices));
 
         return http.build();
+    }
+
+    private void invalidSession(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Session expired or invalid");
+        LOG.info(STR."Session expired or invalid. Request cookie: \{request.getHeader(COOKIE)} accessing \{request.getRequestURI()}");
     }
 
 }
