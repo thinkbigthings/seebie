9a01b7ed7a108e687d35ea9de00d9a19b3fbaf87
Jason Young <thinkbigthings@gmail.com>
2024-11-03 07:44:39 -0500

    clear todos

--- server/src/main/java/com/seebie/server/service/HistogramCalculator.java
+++ server/src/main/java/com/seebie/server/service/HistogramCalculator.java
@@ -22,16 +22,6 @@ public class HistogramCalculator {
      */
     public StackedHistograms buildNormalizedHistogram(final int binSize, final List<List<Long>> multiDataSets) {
 
-        // TODO put the bin calculator somewhere else (use gatherer later?)
-        // Where should it go: Move it and other histogram work into the originating service?
-        // then watch the complexity
-
-        // TODO data type should be short, not long. Long is still signed.
-        // run both ways and watch memory usage for large data sets
-
-        // TODO buildHistogram() should return the list of values that aligns with the list of bins
-        // and put that and normalization as methods in the StackedHistograms class
-
         // build a complete set of bins that can account for all the data sets
         // if there was no data in any of the incoming data sets, the bin list will be the empty set
         LongSummaryStatistics stats = multiDataSets.stream()
