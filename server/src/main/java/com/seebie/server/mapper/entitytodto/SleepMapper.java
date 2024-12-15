e76f1c86fe834c60c0a5c339046924d5d1c11841
Jason Young <thinkbigthings@gmail.com>
2024-12-15 16:18:56 -0500

    refactor constructor

--- server/src/main/java/com/seebie/server/mapper/entitytodto/SleepMapper.java
+++ server/src/main/java/com/seebie/server/mapper/entitytodto/SleepMapper.java
@@ -11,6 +11,7 @@ public class SleepMapper implements Function<SleepSession, SleepDetails> {
     @Override
     public SleepDetails apply(SleepSession entity) {
         return new SleepDetails(entity.getId(), entity.getMinutesAsleep(), entity.getNotes(), entity.getMinutesAwake(),
-                entity.getStartTime(), entity.getStopTime(), ZoneId.of(entity.getZoneId()).getId());
+                entity.getStartTime().toLocalDateTime(), entity.getStopTime().toLocalDateTime(),
+                ZoneId.of(entity.getZoneId()).getId());
     }
 }
