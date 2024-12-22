e0ec50a72af4e17be63059e0c77e255cfbcd64df
Jason Young <thinkbigthings@gmail.com>
2024-12-22 16:15:10 -0500

    fix regression for manual sleep log

--- reactjs/src/utility/SleepDataManager.ts
+++ reactjs/src/utility/SleepDataManager.ts
@@ -1,9 +1,9 @@
 import {SleepData} from "../types/sleep.types";
-import {Duration, LocalDateTime} from "@js-joda/core";
+import {Duration, LocalDate, LocalDateTime, LocalTime} from "@js-joda/core";
 
 const createInitSleepData = ():SleepData => {
 
-    let today = LocalDateTime.now();
+    let today = LocalDateTime.of(LocalDate.now(), LocalTime.of(5, 45, 0))
     let yesterday = today.minusHours(8);
 
     // leave the startTime and stopTime as strings
